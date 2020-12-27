7c7efd189a5aa24ea32d20aaca216284b5a42dbc
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2020-12-26 22:43:51 -0300

    inclusao de testes funcionais

---
+++ src/test/java/br/df/jlimberger/tasks/functional/prod/SmokeTestIT.java
@@ -0,0 +1,29 @@
+package br.df.jlimberger.tasks.functional.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SmokeTestIT {
+	
+	@Test
+	public void smokeTest() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver( new URL("http://192.168.0.114:4444/wd/hub"),cap);
+		try {
+			driver.navigate().to("http://192.168.0.114:9999/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String versao = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(versao.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+	}
+}
